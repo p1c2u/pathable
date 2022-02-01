4c19c07ab4e22994ed0c4d117faa03c1de94f0b7
p1c2u <maciag.artur@gmail.com>
2022-02-01 03:02:07 +0000

    Version 0.3.0

--- pathable/__init__.py
+++ pathable/__init__.py
@@ -8,7 +8,7 @@ from pathable.paths import (
 
 __author__ = 'Artur Maciag'
 __email__ = 'maciag.artur@gmail.com'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 __url__ = 'https://github.com/p1c2u/pathable'
 __license__ = 'Apache License, Version 2.0'
 
