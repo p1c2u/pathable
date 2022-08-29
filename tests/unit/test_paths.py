8ddc400b0b8612edce2199f38afb944524b5ee00
p1c2u <maciag.artur@gmail.com>
2022-08-29 10:51:14 +0100

    add typing

--- tests/unit/test_paths.py
+++ tests/unit/test_paths.py
@@ -2,7 +2,7 @@ from types import GeneratorType
 
 import pytest
 
-from pathable.paths import SEPARATOR
+from pathable.parsers import SEPARATOR
 from pathable.paths import BasePath
 from pathable.paths import LookupPath
 
@@ -76,82 +76,147 @@ class TestBasePathInit:
         assert p.separator == separator
 
 
-class TestBasePathFromParts:
-    def test_default(self):
-        parts = []
-        p = BasePath._from_parts(parts)
+class TestBasePathStr:
+    def test_empty(self):
+        p = BasePath()
 
-        assert p.parts == parts
-        assert p.separator == SEPARATOR
+        assert str(p) == ""
 
-    def test_parts(self):
-        parts = ["part1"]
-        p = BasePath._from_parts(parts)
+    def test_single(self):
+        p = BasePath("part1")
 
-        assert p.parts == parts
-        assert p.separator == SEPARATOR
+        assert str(p) == "part1"
 
-    def test_parts_unparsed(self):
-        parts = ["part1", "part2"]
-        part = SEPARATOR.join(parts)
-        p = BasePath._from_parts([part])
+    def test_double(self):
+        args = ["part1", "part2"]
+        p = BasePath(*args)
 
-        assert p.parts == parts
-        assert p.separator == SEPARATOR
-
-    def test_parts_many(self):
-        parts = ["part1", "part2"]
-        p = BasePath._from_parts(parts)
-
-        assert p.parts == parts
-        assert p.separator == SEPARATOR
+        assert str(p) == SEPARATOR.join(args)
 
     def test_separator(self):
-        parts = []
-        separator = "."
-        p = BasePath._from_parts(parts, separator=separator)
+        args = ["part1", "part2"]
+        separator = ","
+        p = BasePath(*args, separator=separator)
 
-        assert p.parts == parts
-        assert p.separator == separator
+        assert str(p) == separator.join(args)
+
+    def test_cparts_cached(self):
+        args = ["part1", "part2"]
+        separator = ","
+        p = BasePath(*args, separator=separator)
+        p._cparts_cached = p._get_cparts()
+
+        assert str(p) == separator.join(args)
 
 
-class TestBasePathFromParsedParts:
-    def test_default(self):
-        parts = []
-        p = BasePath._from_parsed_parts(parts)
+class TestBasePathRepr:
+    def test_empty(self):
+        p = BasePath()
 
-        assert p.parts == parts
-        assert p.separator == SEPARATOR
+        assert repr(p) == "BasePath('')"
 
-    def test_parts(self):
-        parts = ["part1"]
-        p = BasePath._from_parsed_parts(parts)
+    def test_single(self):
+        arg = "part1"
+        p = BasePath(arg)
 
-        assert p.parts == parts
-        assert p.separator == SEPARATOR
+        assert repr(p) == f"BasePath('{arg}')"
 
-    def test_parts_unparsed(self):
-        part = SEPARATOR.join(["part1", "part2"])
-        parts = [part]
-        p = BasePath._from_parsed_parts(parts)
+    def test_double(self):
+        args = ["part1", "part2"]
+        p = BasePath(*args)
 
-        assert p.parts == parts
-        assert p.separator == SEPARATOR
-
-    def test_parts_many(self):
-        parts = ["part1", "part2"]
-        p = BasePath._from_parsed_parts(parts)
-
-        assert p.parts == parts
-        assert p.separator == SEPARATOR
+        p_str = SEPARATOR.join(args)
+        assert repr(p) == f"BasePath('{p_str}')"
 
     def test_separator(self):
-        parts = []
-        separator = "."
-        p = BasePath._from_parsed_parts(parts, separator=separator)
+        args = ["part1", "part2"]
+        separator = ","
+        p = BasePath(*args, separator=separator)
+
+        p_str = separator.join(args)
+        assert repr(p) == f"BasePath('{p_str}')"
+
+
+class TestBasePathHash:
+    def test_empty(self):
+        p = BasePath()
+
+        assert hash(p) == hash(tuple(p._cparts))
+
+    def test_single(self):
+        p = BasePath("part1")
+
+        assert hash(p) == hash(tuple(p._cparts))
+
+    def test_double(self):
+        args = ["part1", "part2"]
+        p = BasePath(*args)
+
+        assert hash(p) == hash(tuple(p._cparts))
+
+    def test_separator(self):
+        args = ["part1", "part2"]
+        separator = ","
+        p = BasePath(*args, separator=separator)
+
+        assert hash(p) == hash(tuple(p._cparts))
+
+    def test_cparts_cached(self):
+        args = ["part1", "part2"]
+        separator = ","
+        p = BasePath(*args, separator=separator)
+        p._cparts_cached = p._get_cparts()
+
+        assert hash(p) == hash(tuple(p._cparts))
+
+
+class TestBasePathMakeChild:
+    @pytest.fixture
+    def base_path(self):
+        return BasePath(separator=SEPARATOR)
+
+    def test_none(self, base_path):
+        args = []
+        p = base_path._make_child(args)
+
+        assert p.parts == args
+        assert p.separator == SEPARATOR
+
+    def test_arg(self, base_path):
+        args = ["part1"]
+        p = base_path._make_child(args)
+
+        assert p.parts == args
+        assert p.separator == SEPARATOR
+
+    def test_arg_unparsed(self, base_path):
+        parts = ["part1", "part2"]
+        arg = SEPARATOR.join(parts)
+        args = [arg]
+        p = base_path._make_child(args)
 
         assert p.parts == parts
-        assert p.separator == separator
+        assert p.separator == SEPARATOR
+
+    def test_args_many(self, base_path):
+        args = ["part1", "part2"]
+        p = base_path._make_child(args)
+
+        assert p.parts == args
+        assert p.separator == SEPARATOR
+
+
+class TestBasePathMakeChildRelPath:
+    @pytest.fixture
+    def base_path(self):
+        return BasePath(separator=SEPARATOR)
+
+    def test_part(self, base_path):
+        part = "part1"
+        p = base_path._make_child_relpath(part)
+
+        assert p.parts == [part]
+        assert p.separator == SEPARATOR
 
 
 class TestBasePathTruediv:
@@ -264,6 +329,10 @@ class TestBasePathTruediv:
         assert p.parts == [part11, part12, part21, part22]
         assert p.separator == separator1
 
+    def test_type_not_implemented(self):
+        with pytest.raises(TypeError):
+            BasePath() / []
+
 
 class TestBasePathRtruediv:
     def test_default_empty(self):
@@ -361,6 +430,10 @@ class TestBasePathRtruediv:
         assert p.parts == [part11, part12, part21, part22]
         assert p.separator == SEPARATOR
 
+    def test_type_not_implemented(self):
+        with pytest.raises(TypeError):
+            [] / BasePath()
+
 
 class TestBasePathEq:
     @pytest.mark.parametrize(
@@ -380,8 +453,144 @@ class TestBasePathEq:
 
         assert result is expected
 
+    def test_type_not_implemented(self):
+        result = BasePath() == []
 
-class TestLookupPathPathGetItem:
+        assert result is False
+
+
+class TestBasePathLt:
+    @pytest.mark.parametrize(
+        "part1,part2,expected",
+        (
+            ["", "", False],
+            ["", "part", True],
+            ["part", "", False],
+            ["part", "part", False],
+            ["part", "part2", True],
+            ["part", BasePath("part"), False],
+            ["part", BasePath("part2"), True],
+            [BasePath("part"), "part", False],
+            [BasePath("part"), BasePath("part"), False],
+            [BasePath("part"), BasePath("part2"), True],
+        ),
+    )
+    def test_parts(self, part1, part2, expected):
+        result = BasePath(part1) < BasePath(part2)
+
+        assert result is expected
+
+    def test_type_not_implemented(self):
+        with pytest.raises(TypeError):
+            BasePath() < []
+
+
+class TestBasePathLe:
+    @pytest.mark.parametrize(
+        "part1,part2,expected",
+        (
+            ["", "", True],
+            ["", "part", True],
+            ["part", "", False],
+            ["part", "part", True],
+            ["part", "part2", True],
+            ["part", BasePath("part"), True],
+            ["part", BasePath("part2"), True],
+            [BasePath("part"), "part", True],
+            [BasePath("part"), BasePath("part"), True],
+            [BasePath("part"), BasePath("part2"), True],
+        ),
+    )
+    def test_parts(self, part1, part2, expected):
+        result = BasePath(part1) <= BasePath(part2)
+
+        assert result is expected
+
+    def test_type_not_implemented(self):
+        with pytest.raises(TypeError):
+            BasePath() <= []
+
+
+class TestBasePathGt:
+    @pytest.mark.parametrize(
+        "part1,part2,expected",
+        (
+            ["", "", False],
+            ["", "part", False],
+            ["part", "", True],
+            ["part", "part", False],
+            ["part", "part2", False],
+            ["part", BasePath("part"), False],
+            ["part", BasePath("part2"), False],
+            [BasePath("part"), "part", False],
+            [BasePath("part"), BasePath("part"), False],
+            [BasePath("part"), BasePath("part2"), False],
+        ),
+    )
+    def test_parts(self, part1, part2, expected):
+        result = BasePath(part1) > BasePath(part2)
+
+        assert result is expected
+
+    def test_type_not_implemented(self):
+        with pytest.raises(TypeError):
+            BasePath() > []
+
+
+class TestBasePathGe:
+    @pytest.mark.parametrize(
+        "part1,part2,expected",
+        (
+            ["", "", True],
+            ["", "part", False],
+            ["part", "", True],
+            ["part", "part", True],
+            ["part", "part2", False],
+            ["part", BasePath("part"), True],
+            ["part", BasePath("part2"), False],
+            [BasePath("part"), "part", True],
+            [BasePath("part"), BasePath("part"), True],
+            [BasePath("part"), BasePath("part2"), False],
+        ),
+    )
+    def test_parts(self, part1, part2, expected):
+        result = BasePath(part1) >= BasePath(part2)
+
+        assert result is expected
+
+    def test_type_not_implemented(self):
+        with pytest.raises(TypeError):
+            BasePath() >= []
+
+
+class TestLookupPathIter:
+    def test_object(self):
+        resource = {"test1": {"test2": {"test3": "test"}}}
+        p = LookupPath(resource, "test1/test2")
+
+        result = iter(p)
+
+        assert type(result) == GeneratorType
+        result_list = list(result)
+        assert result_list == [
+            LookupPath(resource, "test1/test2/0"),
+        ]
+
+    def test_list(self):
+        resource = {"test1": {"test2": ["test3", "test4"]}}
+        p = LookupPath(resource, "test1/test2")
+
+        result = iter(p)
+
+        assert type(result) == GeneratorType
+        result_list = list(result)
+        assert result_list == [
+            LookupPath(resource, "test1/test2/0"),
+            LookupPath(resource, "test1/test2/1"),
+        ]
+
+
+class TestLookupPathGetItem:
     def test_valid(self):
         value = "testvalue"
         resource = {"test1": {"test2": {"test3": value}}}
@@ -400,7 +609,135 @@ class TestLookupPathPathGetItem:
             p["test4"]
 
 
-class TestLookupPathPathContains:
+class TestLookupPathGetkey:
+    def test_valid(self):
+        value = "testvalue"
+        resource = {"test1": {"test2": {"test3": value}}}
+        p = LookupPath(resource, "test1/test2")
+
+        result = p.getkey("test3")
+
+        assert result == value
+
+    def test_invalid(self):
+        value = "testvalue"
+        resource = {"test1": {"test2": {"test3": value}}}
+        p = LookupPath(resource, "test1/test2")
+
+        result = p.getkey("test4")
+
+        assert result is None
+
+
+class TestLookupPathGet:
+    def test_non_existing_key_default_none(self):
+        value = "testvalue"
+        resource = {"test1": {"test2": {"test3": value}}}
+        p = LookupPath(resource, "test1/test2")
+
+        result = p.get("")
+
+        assert result == None
+
+    def test_non_existing_key_default_defined(self):
+        value = "testvalue"
+        resource = {"test1": {"test2": {"test3": value}}}
+        p = LookupPath(resource, "test1/test2")
+
+        result = p.get("", default=value)
+
+        assert result == value
+
+    @pytest.mark.parametrize(
+        "resource,key,expected",
+        (
+            [
+                {"test1": "test2"},
+                "test1",
+                LookupPath({"test1": "test2"}, "test1"),
+            ],
+            [
+                {"test1": {"test2": "test3"}},
+                "test1",
+                LookupPath({"test1": {"test2": "test3"}}, "test1"),
+            ],
+        ),
+    )
+    def test_key_exists(self, resource, key, expected):
+        p = LookupPath(resource)
+
+        result = p.get(key)
+
+        assert result == expected
+
+
+class TestLookupPathLen:
+    def test_empty(self):
+        resource = {}
+        p = LookupPath(resource)
+
+        result = len(p)
+
+        assert result == 0
+
+    def test_value(self):
+        resource = {"test1": "test2"}
+        p = LookupPath(resource, "test1")
+
+        result = len(p)
+
+        assert result == 5
+
+    def test_single(self):
+        resource = {"test1": "test2"}
+        p = LookupPath(resource)
+
+        result = len(p)
+
+        assert result == 1
+
+    def test_list(self):
+        resource = {"test1": [{"test2": "test3"}, {"test4": "test5"}]}
+        p = LookupPath(resource, "test1")
+
+        result = len(p)
+
+        assert result == 2
+
+
+class TestLookupPathKeys:
+    def test_empty(self):
+        resource = {}
+        p = LookupPath(resource)
+
+        result = p.keys()
+
+        assert list(result) == []
+
+    def test_value(self):
+        resource = {"test1": "test2"}
+        p = LookupPath(resource, "test1")
+
+        with pytest.raises(AttributeError):
+            p.keys()
+
+    def test_single(self):
+        resource = {"test1": "test2"}
+        p = LookupPath(resource)
+
+        result = p.keys()
+
+        assert list(result) == ["test1"]
+
+    def test_list(self):
+        resource = {"test1": [{"test2": "test3"}, {"test4": "test5"}]}
+        p = LookupPath(resource, "test1")
+
+        with pytest.raises(AttributeError):
+            p.keys()
+
+
+class TestLookupPathContains:
     def test_valid(self):
         value = "testvalue"
         resource = {"test1": {"test2": {"test3": value}}}
@@ -420,7 +757,7 @@ class TestLookupPathPathContains:
         assert result is False
 
 
-class TestLookupPathPathItems:
+class TestLookupPathItems:
     def test_empty(self):
         resource = {}
         p = LookupPath(resource)
@@ -444,3 +781,20 @@ class TestLookupPathPathItems:
             "test1": p / "test1",
             "test2": p / "test2",
         }
+
+
+class TestLookupPathOpen:
+    def test_content_cached(self):
+        value = {
+            "test3": "test4",
+        }
+        resource = {
+            "test1": 1,
+            "test2": value,
+        }
+        p = LookupPath(resource, "test2")
+
+        result = p.getkey("test3")
+        assert result == p.getkey("test3")
+
+        assert p._content_cached == value
