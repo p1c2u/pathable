41da0f6ecc901fa69f0568624a637289b052b7f8
p1c2u <maciag.artur@gmail.com>
2025-06-07 21:44:18 +0100

    Version 0.5.0b2

--- pathable/__init__.py
+++ pathable/__init__.py
@@ -7,7 +7,7 @@ from pathable.paths import LookupPath as ListPath
 
 __author__ = "Artur Maciag"
 __email__ = "maciag.artur@gmail.com"
-__version__ = "0.5.0b1"
+__version__ = "0.5.0b2"
 __url__ = "https://github.com/p1c2u/pathable"
 __license__ = "Apache License, Version 2.0"
 
