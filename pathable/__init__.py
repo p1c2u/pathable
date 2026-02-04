1f8d5a035054241760c013805d445eabded203af
p1c2u <maciag.artur@gmail.com>
2026-02-04 20:23:19 +0000

    Export FilesystemPath and accessors

--- pathable/__init__.py
+++ pathable/__init__.py
@@ -1,6 +1,9 @@
 """Pathable module"""
+from pathable.accessors import NodeAccessor
+from pathable.accessors import PathAccessor
 from pathable.paths import AccessorPath
 from pathable.paths import BasePath
+from pathable.paths import FilesystemPath
 from pathable.paths import LookupPath
 from pathable.paths import LookupPath as DictPath
 from pathable.paths import LookupPath as ListPath
@@ -14,7 +17,10 @@ __license__ = "Apache License, Version 2.0"
 __all__ = [
     "BasePath",
     "AccessorPath",
+    "FilesystemPath",
     "LookupPath",
     "DictPath",
     "ListPath",
+    "NodeAccessor",
+    "PathAccessor",
 ]
